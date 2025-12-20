5aa74de5d201eb0dbebe9a5241837b566a685cd9
Artem danilov <zaxe2016@bk.ru>
2025-12-20 11:12:28 +1000

    save fales2

--- introduction/introduction.go
+++ introduction/introduction.go
@@ -2,6 +2,14 @@ package introduction
 
 import "fmt"
 
-func Conduction() {
-	fmt.Print("Ведите первое число !")
+func Conduction1() {
+	fmt.Print("Ведите 1-ое число =")
+	var num1 float64
+	fmt.Scan(&num1)
+}
+
+func Conduction2() {
+	fmt.Print("Ведите 2-ое число =")
+	var num2 float64
+	fmt.Scan(&num2)
 }
--- main.go
+++ main.go
@@ -3,8 +3,6 @@ package main
 import "calculator/introduction"
 
 func main() {
-	//var name string
-	//fmt.Print("Ведитн ваше имя ")
-	//fmt.Scan(&name)
-	introduction.Conduction()
+	introduction.Conduction1()
+	introduction.Conduction2()
 }
