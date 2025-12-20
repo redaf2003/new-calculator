d7a7203de1bd8b008c1acb8a2e4708df4ef4cc2d
Artem danilov <zaxe2016@bk.ru>
2025-12-20 16:32:15 +1000

    adding a new paremeter

--- introduction/introduction.go
+++ introduction/introduction.go
@@ -7,8 +7,13 @@ func Conduction1() {
 	var num1 float64 //-Пользователь пишет число в консоль (вся функция)
 	fmt.Scan(&num1)
 }
-
 func Conduction2() {
+	fmt.Print("Введите операцию (+, -, *, /): ")
+	var operator float64
+	fmt.Scan(&operator)
+}
+
+func Conduction3() {
 	fmt.Print("Ведите 2-ое число =")
 	var num2 float64 //-Пользователь пишет 2-ое число в консоль (вся функция)
 	fmt.Scan(&num2)
--- main.go
+++ main.go
@@ -5,4 +5,5 @@ import "calculator/introduction"
 func main() {
 	introduction.Conduction1()
 	introduction.Conduction2()
+	introduction.Conduction3()
 }
