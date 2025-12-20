9808ce4d184e92cf8460a845b39e1b5415a3968a
Artem danilov <zaxe2016@bk.ru>
2025-12-20 11:18:52 +1000

    save fale3

--- introduction/introduction.go
+++ introduction/introduction.go
@@ -4,12 +4,12 @@ import "fmt"
 
 func Conduction1() {
 	fmt.Print("Ведите 1-ое число =")
-	var num1 float64
+	var num1 float64 //-Пользователь пишет число в консоль (вся функция)
 	fmt.Scan(&num1)
 }
 
 func Conduction2() {
 	fmt.Print("Ведите 2-ое число =")
-	var num2 float64
+	var num2 float64 //-Пользователь пишет 2-ое число в консоль (вся функция)
 	fmt.Scan(&num2)
 }
