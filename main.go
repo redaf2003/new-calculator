8b4e8650e016e85dc426df27d89d67d86a9f17e4
Artem danilov <zaxe2016@bk.ru>
2025-12-26 19:22:18 +1000

    adding a new paremeter5

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	pointers.Pointers()
+	pointers.Numbersss()
 	introduction.RunCalculator()
 
 }
--- pointers/pointers.go
+++ pointers/pointers.go
@@ -1,14 +1,26 @@
 package pointers //-указатели
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func Pointers() {
 	message := "Я тупой егор"
-	MessageGay(message)
-	//fmt.Println(message)
+	MessageGay(&message)
+	fmt.Println(message)
 }
 
-func MessageGay(message string) {
-	message += "(Да я тупой егор сори !)"
+func MessageGay(message *string) {
+	*message += "(Да я тупой егор сори!)"
 	fmt.Println(message)
+}
+
+func Numbersss() {
+	number := 5
+	p := &number
+	fmt.Println(number)
+	fmt.Println(p)
+
+	*p = 10
+	fmt.Println(number)
 }
