dbc7a5abfaa9e4575bd6e36f5c6ac3f4c6b2bd9a
Artem danilov <zaxe2016@bk.ru>
2026-01-21 03:39:38 +1000

    adding a new paremete7

--- slaic/slaicc.go
+++ slaic/slaicc.go
@@ -8,8 +8,11 @@ func FuxMake() {
 	fmt.Println("")
 	fmt.Println("Функция майк:")
 	message := make([]string, 5)
+	message = append(message, "6")
+	//message[6] = "6" //-нельзя так делать
 	fmt.Println("Длина:", len(message))   //-длина слайса, len()
 	fmt.Println("Емкость:", cap(message)) //-емкость нашего слайса cap()
+	fmt.Println(message)
 	//fmt.Println(message)
 	fmt.Println("")
 }
