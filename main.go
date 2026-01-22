8ae592cc32d70e9be73b233ad885f2a8abd03b9c
Artem danilov <zaxe2016@bk.ru>
2026-01-22 15:36:54 +1000

    adding a new paremete8

--- main.go
+++ main.go
@@ -4,12 +4,15 @@ import (
 	introduction "calculator/introductions"
 	"calculator/pointers"
 	"calculator/slaic"
+	"calculator/variables"
 )
 
 func main() {
+	variables.TheFlyingBird()
 	slaic.Slice()
 	slaic.FuxMake()
 	pointers.Numbersss()
 	introduction.RunCalculator()
+	//variables.TheFlyingBird()
 
 }
